3854685cfcbda59617a4849b54de4d8d3344dc70
Duncan Mac-Vicar P <dmacvicar@suse.de>
2018-12-01 00:41:23 +0100

    Refactor HTTP code into its own reader

---
+++ libvirt/http/http.go
@@ -0,0 +1,140 @@
+package http
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+var ErrNotModified = errors.New("http: Not modified")
+
+// The HTTP reader allows to threat the full request-response cycle
+// as a reader.
+type Reader struct {
+	url             string
+	response        *http.Response
+	ifModifiedSince *time.Time
+}
+
+func (r *Reader) String() string {
+	return r.url
+}
+
+// The reader will only download data if it was modified
+// server-side after the given time.
+//
+// If the data has not been modified since, reading will
+// return a ErrNotModified error.
+func (r *Reader) SetIfModifiedSince(t time.Time) {
+	*r.ifModifiedSince = t
+}
+
+// Returns the length of the data according to server-side
+func (r *Reader) Size() (uint64, error) {
+	response, err := http.Head(r.url)
+	if err != nil {
+		return 0, err
+	}
+	if response.StatusCode == 403 {
+		// possibly only the HEAD method is forbidden, try a Body-less GET instead
+		response, err = http.Get(r.url)
+		if err != nil {
+			return 0, err
+		}
+
+		response.Body.Close()
+	}
+	if response.StatusCode != 200 {
+		return 0,
+			fmt.Errorf(
+				"Error accessing remote resource: %s - %s",
+				r.url,
+				response.Status)
+	}
+
+	length, err := strconv.Atoi(response.Header.Get("Content-Length"))
+	if err != nil {
+		err = fmt.Errorf(
+			"Error while getting Content-Length of \"%s\": %s - got %s",
+			r.url,
+			err,
+			response.Header.Get("Content-Length"))
+		return 0, err
+	}
+	return uint64(length), nil
+}
+
+func NewReader(url string) (*Reader, error) {
+	return &Reader{url: url}, nil
+}
+
+//func (b *Reader) Peek(n int) ([]byte, error) {
+//}
+
+func (r *Reader) Read(p []byte) (int, error) {
+	err := r.doRequest()
+	if err != nil {
+		return 0, err
+	}
+	return r.response.Body.Read(p)
+}
+
+func (r *Reader) Close() error {
+	if r.response != nil {
+		return r.response.Body.Close()
+	}
+	return nil
+}
+
+func (r *Reader) doRequest() error {
+	if r.response != nil {
+		return nil
+	}
+
+	// number of download retries on non client errors (eg. 5xx)
+	const maxHTTPRetries int = 3
+	// wait time between retries
+	const retryWait time.Duration = 2 * time.Second
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", r.url, nil)
+
+	if err != nil {
+		log.Printf("[DEBUG:] Error creating new request for source url %s: %s", r.url, err)
+		return fmt.Errorf("Error while downloading %s: %s", r.url, err)
+	}
+
+	if r.ifModifiedSince != nil {
+		req.Header.Set("If-Modified-Since", r.ifModifiedSince.UTC().Format(http.TimeFormat))
+	}
+
+	var response *http.Response
+	for retryCount := 0; retryCount < maxHTTPRetries; retryCount++ {
+		response, err = client.Do(req)
+		if err != nil {
+			return fmt.Errorf("Error while downloading %s: %v", r.url, err)
+		}
+
+		log.Printf("[DEBUG]: url resp status code %s (retry #%d)\n", response.Status, retryCount)
+		if response.StatusCode == http.StatusNotModified {
+			response.Body.Close()
+			return ErrNotModified
+		} else if response.StatusCode == http.StatusOK {
+			r.response = response
+			return nil
+		} else if response.StatusCode < 500 {
+			break
+		} else {
+			// The problem is not client but server side
+			// retry a few times after a small wait
+			if retryCount < maxHTTPRetries {
+				time.Sleep(retryWait)
+			}
+		}
+	}
+	response.Body.Close()
+	return fmt.Errorf("Error while downloading %s: %v", r.url, response)
+}
---
+++ libvirt/http/http_test.go
@@ -0,0 +1,75 @@
+package http
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRemoteImageDownloadRetry(t *testing.T) {
+	content := []byte("this is a qcow image... well, it is not")
+
+	// returns a server that returns every error from
+	// errorList before returning a valid response
+	newErrorServer := func(errorList []int) *httptest.Server {
+		errorCount := 0
+		return httptest.NewServer(
+			http.HandlerFunc(
+				func(w http.ResponseWriter, r *http.Request) {
+					if errorCount < len(errorList) {
+						t.Logf("Server serving retry %d", errorCount)
+						http.Error(w, fmt.Sprintf("Error %d", errorCount), errorList[errorCount])
+						errorCount = errorCount + 1
+					} else {
+						t.Logf("Server: success (after %d errors)", errorCount)
+						http.ServeContent(w, r, "content", time.Now(), bytes.NewReader(content))
+					}
+				}))
+	}
+
+	server := newErrorServer([]int{503, 503})
+	defer server.Close()
+
+	start := time.Now()
+	reader, err := NewReader(server.URL)
+	if err != nil {
+		t.Errorf("Could not create an HTTP reader: %v", err)
+	}
+	defer reader.Close()
+
+	data, err := ioutil.ReadAll(reader)
+	if err != nil {
+		t.Fatalf("Expected to retry: %v", err)
+	}
+	if time.Since(start).Seconds() < 4 {
+		t.Fatalf("Expected to retry at least 2 times x 2 seconds")
+	}
+	if !bytes.Equal(data, content) {
+		t.Fatalf("Expected reading %v", string(content))
+	}
+
+	server = newErrorServer([]int{503, 404})
+	defer server.Close()
+
+	start = time.Now()
+	reader, err = NewReader(server.URL)
+	if err != nil {
+		t.Errorf("Could not create an HTTP reader: %v", err)
+	}
+	defer reader.Close()
+
+	data, err = ioutil.ReadAll(reader)
+	if err == nil {
+		t.Fatalf("Expected %s to fail with status 4xx", server.URL)
+	}
+	if time.Since(start).Seconds() < 2 {
+		t.Fatalf("Expected to retry at least 1 times x 2 seconds")
+	}
+	if len(data) != 0 {
+		t.Fatalf("Expected not reading anything")
+	}
+}
